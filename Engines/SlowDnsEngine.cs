22848472efac213b0b4eec0f84c7be56c86c3126
kinf744 <adrienyuoi@gmail.com>
2026-06-24 02:05:17 +0100

    fix: add missing using System.Collections.Generic in SlowDnsEngine

--- Engines/SlowDnsEngine.cs
+++ Engines/SlowDnsEngine.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace KighmuVpnWindows.Engines
