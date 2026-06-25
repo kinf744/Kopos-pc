2d3b555b7e80b7b919c115272b85ba62b0bbbb3e
kinf744 <adrienyuoi@gmail.com>
2026-06-25 14:40:01 +0100

    fix: enlever protocol:udp par défaut, garder portHopping 20000-50000

--- Models/HysteriaConfig.cs
+++ Models/HysteriaConfig.cs
@@ -13,7 +13,7 @@ namespace KighmuVpnWindows.Models
         [JsonProperty("uploadMbps")] public int UploadMbps { get; set; } = 10;
         [JsonProperty("downloadMbps")] public int DownloadMbps { get; set; } = 50;
         [JsonProperty("obfsPassword")] public string ObfsPassword { get; set; } = "";
-        [JsonProperty("portHopping")] public string PortHopping { get; set; } = "";
+        [JsonProperty("portHopping")] public string PortHopping { get; set; } = "20000-50000";
         [JsonProperty("protocol")] public string Protocol { get; set; } = "";
     }
 }
